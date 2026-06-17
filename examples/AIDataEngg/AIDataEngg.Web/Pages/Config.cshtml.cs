03769a260091dd9627fc63e2b513023164c4c4ee
khurram-uworx <khurram.aziz@uworx.co.uk>
2026-06-17 17:54:13 +0500

    cleanup

--- examples/AIDataEngg/AIDataEngg.Web/Pages/Config.cshtml.cs
+++ examples/AIDataEngg/AIDataEngg.Web/Pages/Config.cshtml.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using Streamix.AIDataEngg.Data;
 using Streamix.AIDataEngg.Models;
 using Streamix.AIDataEngg.Services;
